b7edc645a79b6531c60e65e559a906c67451cc2e
Mark Dewing <markdewing@gmail.com>
2025-04-09 13:07:07 -0500

    Conform basic test to current class structure

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -2,16 +2,15 @@
 import numpy as np
 import pytest
 
-from hartree_fock.basis import GaussianBasisFunction
-from cubature.integrator import CubatureIntegrator
+from basis import GaussianBasis
 
 def test_gaussian_basis_evaluation():
     center = np.array([0.0, 0.0, 0.0])
     alpha = 1.0
-    basis = GaussianBasisFunction(center=center, params={"alpha": alpha})
+    basis = GaussianBasis(center=center, alpha=alpha, normalize=False)
 
     x = np.array([0.0, 0.0, 0.0])
-    value = basis.evaluate(x)
+    value = basis(x)
 
     assert np.isclose(value, 1.0), "Gaussian should be 1.0 at its center"
 
